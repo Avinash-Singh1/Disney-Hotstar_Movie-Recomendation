1d29313e9b2b62a9107bb835e1fc04cdbd365e79
Avinash-singh1 <avinash202002@gmail.com>
2024-11-02 10:24:50 +0530

    Added changes for azure open ai studio

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,25 @@
-import { Button } from "./components/ui/button";
-import { SignInButton } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
-import Image from "next/image";
-import Link from "next/link";
+import CarouselBannerWrapper from "@/components/CarouselBannerWrapper";
+import MoviesCarousel from "@/components/MoviesCarousel";
+import {
+  getPopularMovies,
+  getTopRatedMovies,
+  getUpcomingMovies,
+} from "@/lib/getMovies";
 
 export default async function Home() {
-  const { userId } = await auth();
+  const upcomingMovies = await getUpcomingMovies();
+  const topRatedMovies = await getTopRatedMovies();
+  const popularMovies = await getPopularMovies();
 
   return (
-    <main className="flex flex-col items-center justify-center p-10">
-      <h1 className="text-3xl lg:text-6xl text-center pb-10 mb-5 font-light">
-        Understand your world and communicate across languages
-      </h1>{" "}
-      <Image
-        src="https://links.papareact.com/ert"
-        alt="logo"
-        width={700}
-        height={700}
-      />
-      {userId ? (
-        <Link
-          href="/translate"
-          className="bg-blue-500 hover:bg-blue-600 w-full mt-10 lg:w-fit p-5 rounded-md text-white text-center cursor-pointer"
-        >
-          Translate Now
-        </Link>
-      ) : (
-        <Button className="bg-blue-500 hover:bg-blue-600 w-full mt-10 lg:w-fit p-5">
-          <SignInButton  mode="modal">
-            Sign In to Get Translating
-          </SignInButton>
-        </Button>
-      )}
+    <main className="">
+      <CarouselBannerWrapper />
+
+      <div className="flex flex-col space-y-2 xl:-mt-48">
+        <MoviesCarousel movies={upcomingMovies} title="Upcoming" />
+        <MoviesCarousel movies={topRatedMovies} title="Top Rated" />
+        <MoviesCarousel movies={popularMovies} title="Popular" />
+      </div>
     </main>
   );
 }
